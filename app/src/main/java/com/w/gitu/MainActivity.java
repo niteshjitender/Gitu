fc67074edf6dcc58b3a98e845a64f972c3738ef2
niteshjitender <niteshjitender@gmail.com>
2019-09-23 12:52:56 +0530

    manisha branch 3 mein change kar diya

--- app/src/main/java/com/w/gitu/MainActivity.java
+++ app/src/main/java/com/w/gitu/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    private void xcdhsb()
-    {
-        int i=3;
+    void lundpakkad(){
+        String s = "gaand maraaa !!!" ;
     }
 }
